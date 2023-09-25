071ac534695992f0fe004a9d437a1de1a22b9ed3
Dmitriy N. Borisov <borik.internet@gmail.com>
2023-09-25 23:26:10 +0300

    Add check_kamailio_stat scripts and services Add restart: on-failure to docker compose services

---
+++ env/icinga2/check_kamailio_stat_below.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+CONTAINER=$1
+STATISTIC=$2
+WARN_LEVEL=$3
+CRIT_LEVEL=$4
+
+VALUE=`/usr/bin/docker exec $CONTAINER /usr/sbin/kamcmd stats.get_statistics $STATISTIC | /bin/sed 's:.*=\s*\([0123456789]\+\):\1:g'`
+
+if [ $VALUE -z ];
+  echo "UNKNOWN: no output statistic $STATISTIC"
+  exit 3
+fi
+
+if [ $VALUE -lt $CRIT_LEVEL ];
+  echo "CRITICAL: statistic $STATISTIC below critical level: $VALUE"
+  exit 2
+fi
+
+if [ $VALUE -lt $WARN_LEVEL ];
+  echo "WARNING: statistic $STATISTIC below warning level: $VALUE"
+  exit 1
+fi
+
+if [ $VALUE -ge $WARN_LEVEL ];
+  echo "OK: statistic $STATISTIC above warning level: $VALUE"
+  exit 0
+fi
+
+echo "UNKNOWN: can't parse get_statistic output: $VALUE"
+exit 3
